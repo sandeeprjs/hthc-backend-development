27adb9db349eef3f2aaacb206accea9b37c89339
sandeep <sandeep.raju@aowealth.in>
2024-11-02 04:51:58 +0530

    Initial commit

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://hthc-web-986807843607.asia-south1.run.app'),
 
     'asset_url' => env('ASSET_URL', null),
 

5aa683cd8c1062a40050325ed5f7bae1699a6275
sandeep <sandeep.raju@aowealth.in>
2025-01-27 18:58:54 +0530

    uat fixed ECR path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,26 +9,42 @@
 
     <title>HTHC</title>
 
-    <link rel="icon" href="{!! secure_asset('images/favicon.ico') !!}"/>
-    <!-- Scripts -->
+    <link rel="icon" href="{{ secure_asset('images/favicon.ico') }}"/>
 
+    <!-- Styles -->
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
     <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- jQuery and Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ secure_asset('js/custom.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,400&display=swap" rel="stylesheet">
-    <!-- Font Awesome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-5e2ESR8Ycmos6g3gAKr1Jvwye8sW4U1u/cAKulfVJnkakCcMqhOudbtPnvJ+nbv7" crossorigin="anonymous">
-
-    <!-- Styles -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
-<!-- Rest of the file remains the same -->
+<div id="app" class="d-flex">
+    @if (Request::is('admin/*'))
+        <div class="navigation">
+            @include('components.nav')
+        </div>
+    @endif
+
+    @hasSection('content')
+        <main class="main-container">
+            @yield('content')
+        </main>
+    @else
+        <main class="container">
+            @yield('auth-content')
+        </main>
+    @endif
+</div>
 </body>
 </html>

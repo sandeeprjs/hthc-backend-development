15446639f7a5940527c2b50cef8ff2a033f32bd7
sandeep <sandeep.raju@aowealth.in>
2025-01-27 18:49:27 +0530

    uat fixed ECR path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,22 +4,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>HTHC</title>
 
-    <link rel="icon" href="{!! asset('images/favicon.ico') !!}"/>
+    <link rel="icon" href="{!! secure_asset('images/favicon.ico') !!}"/>
     <!-- Scripts -->
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-{{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>--}}
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="{{ asset('js/custom.js') }}" defer></script>
+    <script src="{{ secure_asset('js/custom.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -28,39 +25,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-5e2ESR8Ycmos6g3gAKr1Jvwye8sW4U1u/cAKulfVJnkakCcMqhOudbtPnvJ+nbv7" crossorigin="anonymous">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 <body>
-    <div id="app" class="d-flex">
-
-{{--        @if( \Request::route()->getName() != 'login')--}}
-        @if (Request::is('admin/*'))
-            <div class="navigation">
-                @component('components.nav')
-                @endcomponent
-            </div>
-        @endif
-
-
-{{--        @endif--}}
-
-        @hasSection('content')
-            <main class="main-container">
-                @yield('content')
-            </main>
-        @else
-            <main class="container">
-                @yield('auth-content')
-            </main>
-        @endif
-
-
-{{--        <div class="container">--}}
-{{--            @yield('login-content')--}}
-{{--        </div>--}}
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
-
+<!-- Rest of the file remains the same -->
 </body>
 </html>
